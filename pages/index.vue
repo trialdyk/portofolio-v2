45851cd26605e1da1864b3df83c18dfad0dbd9be
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2025-02-28 06:31:13 +0700

    chore : install inspira ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,135 +1,57 @@
 <script setup lang="ts">
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle,
-} from '@/components/ui/navigation-menu'
-
-const components: { title: string, href: string, description: string }[] = [
-    {
-        title: 'Alert Dialog',
-        href: '/docs/components/alert-dialog',
-        description:
-            'A modal dialog that interrupts the user with important content and expects a response.',
-    },
-    {
-        title: 'Hover Card',
-        href: '/docs/components/hover-card',
-        description:
-            'For sighted users to preview content available behind a link.',
-    },
-    {
-        title: 'Progress',
-        href: '/docs/components/progress',
-        description:
-            'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-    },
-    {
-        title: 'Scroll-area',
-        href: '/docs/components/scroll-area',
-        description: 'Visually or semantically separates content.',
-    },
-    {
-        title: 'Tabs',
-        href: '/docs/components/tabs',
-        description:
-            'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-    },
-    {
-        title: 'Tooltip',
-        href: '/docs/components/tooltip',
-        description:
-            'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-    },
-]
+import { CardContainer, CardBody, CardItem } from "~~/components/ui/card-3d";
 </script>
 
 <template>
-    <NavigationMenu>
-        <NavigationMenuList>
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                    <ul class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[minmax(0,.75fr)_minmax(0,1fr)]">
-                        <li class="row-span-3">
-                            <NavigationMenuLink as-child>
-                                <a class="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                    href="/">
-                                    <img src="https://www.reka-ui.com/logo.svg" class="h-6 w-6">
-                                    <div class="mb-2 mt-4 text-lg font-medium">
-                                        shadcn/ui
-                                    </div>
-                                    <p class="text-sm leading-tight text-muted-foreground">
-                                        Beautifully designed components built with Radix UI and
-                                        Tailwind CSS.
-                                    </p>
-                                </a>
-                            </NavigationMenuLink>
-                        </li>
-
-                        <li>
-                            <NavigationMenuLink as-child>
-                                <a href="/docs/introduction"
-                                    class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                                    <div class="text-sm font-medium leading-none">Introduction</div>
-                                    <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                                        Re-usable components built using Radix UI and Tailwind CSS.
-                                    </p>
-                                </a>
-                            </NavigationMenuLink>
-                        </li>
-                        <li>
-                            <NavigationMenuLink as-child>
-                                <a href="/docs/installation"
-                                    class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                                    <div class="text-sm font-medium leading-none">Installation</div>
-                                    <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                                        How to install dependencies and structure your app.
-                                    </p>
-                                </a>
-                            </NavigationMenuLink>
-                        </li>
-                        <li>
-                            <NavigationMenuLink as-child>
-                                <a href="/docs/typography"
-                                    class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                                    <div class="text-sm font-medium leading-none">Typography</div>
-                                    <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                                        Styles for headings, paragraphs, lists...etc
-                                    </p>
-                                </a>
-                            </NavigationMenuLink>
-                        </li>
-                    </ul>
-                </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                    <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                        <li v-for="component in components" :key="component.title">
-                            <NavigationMenuLink as-child>
-                                <a :href="component.href"
-                                    class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                                    <div class="text-sm font-medium leading-none">{{ component.title }}</div>
-                                    <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                                        {{ component.description }}
-                                    </p>
-                                </a>
-                            </NavigationMenuLink>
-                        </li>
-                    </ul>
-                </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-                <NavigationMenuLink href="/docs/introduction" :class="navigationMenuTriggerStyle()">
-                    Documentation
-                </NavigationMenuLink>
-            </NavigationMenuItem>
-        </NavigationMenuList>
-    </NavigationMenu>
-</template>
+    <ClientOnly>
+      <CardContainer>
+        <CardBody
+          class="group/card relative size-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 sm:w-[30rem] dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]"
+        >
+          <CardItem
+            :translate-z="50"
+            class="text-xl font-bold text-neutral-600 dark:text-white"
+          >
+            Make things float in air
+          </CardItem>
+          <CardItem
+            as="p"
+            translate-z="60"
+            class="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300"
+          >
+            Hover over this card to unleash the power of CSS perspective
+          </CardItem>
+          <CardItem
+            :translate-z="100"
+            class="mt-4 w-full"
+          >
+            <NuxtImg
+              src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              height="1000"
+              width="1000"
+              class="h-60 w-full rounded-xl object-cover group-hover/card:shadow-xl"
+              alt="thumbnail"
+            />
+          </CardItem>
+          <div class="mt-20 flex items-center justify-between">
+            <CardItem
+              :translate-z="20"
+              as="a"
+              href="https://rahulv.dev"
+              target="__blank"
+              class="rounded-xl px-4 py-2 text-xs font-normal dark:text-white"
+            >
+              Visit →
+            </CardItem>
+            <CardItem
+              :translate-z="20"
+              as="button"
+              class="rounded-xl bg-black px-4 py-2 text-xs font-bold text-white dark:bg-white dark:text-black"
+            >
+              Get Started
+            </CardItem>
+          </div>
+        </CardBody>
+      </CardContainer>
+    </ClientOnly>
+  </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -50,7 +50,7 @@ export default {
     				'4': 'hsl(var(--chart-4))',
     				'5': 'hsl(var(--chart-5))'
     			}
-    		}
+    		},
     	}
     },
     plugins: [require("tailwindcss-animate")],
