a079bcbe6c4274b69b2f6c2a0960f5e28d5a33c5
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2025-02-28 07:51:59 +0700

    feat : color mode toggle

---
+++ components/global/ColorModeToggle.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <h1>Color mode: {{ colorMode.preference }}</h1>
+      <button 
+        class="rounded-full border border-slate-300 dark:border-slate-700 flex justify-center items-center p-1.5 dark:bg-[#020618] dark:text-white transition-all duration-300"
+        @click="toggleDarkMode"
+      >
+        <Transition name="fade" mode="out-in">
+          <Icon 
+            v-if="colorMode.preference === 'light'" 
+            name="material-symbols:wb-sunny" 
+            class="w-6 h-6 text-yellow-300"
+            key="light"
+          />
+          <Icon 
+            v-else 
+            name="material-symbols:dark-mode" 
+            class="w-6 h-6 text-yellow-300"
+            key="dark"
+          />
+        </Transition>
+      </button>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  const colorMode = useColorMode()
+  
+  const toggleDarkMode = () => {
+    colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+  }
+  </script>
+  
+  <style>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+  }
+  .fade-enter-from, .fade-leave-to {
+    opacity: 0;
+    transform: scale(0.8);
+  }
+  </style>
+  
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,4 +25,15 @@ export default defineNuxtConfig({
     componentDir: './components/ui',
 
   },
+  colorMode:{
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
+    storageKey: 'nuxt-color-mode'
+  }
 })
