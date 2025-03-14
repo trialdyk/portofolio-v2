c736537a75d6b0c238719f703f57127d540bcf1d
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2025-03-14 19:56:39 +0700

    feat : tetris background

---
+++ components/global/ColorModeToggle.vue
@@ -0,0 +1,32 @@
+<template>
+    <button
+        class="rounded-full border border-slate-300 dark:border-slate-700 flex justify-center items-center p-1 dark:bg-[#020618] dark:text-white transition-all duration-300"
+        @click="toggleDarkMode">
+        <Transition name="fade" mode="out-in">
+            <Icon v-if="colorMode.preference === 'light'" name="material-symbols:wb-sunny"
+                class="size-5 text-yellow-500" key="light" />
+            <Icon v-else name="material-symbols:dark-mode" class="size-5 text-yellow-300" key="dark" />
+        </Transition>
+    </button>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const toggleDarkMode = () => {
+    colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+    transform: scale(0.8);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="w-full">
+    <div class="w-full min-h-screen relative">
+        <BackgroundTetris
+            class="fixed inset-0 -z-10 w-full h-full [mask-image:radial-gradient(750px_circle_at_center,#007BFF,transparent)]"
+            :base="30"
+            square-color="#007BFF"
+        />
         <LandingHero />
         <LandingQuote />
         <LandingMySkills />
